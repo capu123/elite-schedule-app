7601d6f045acc47c2f78f914d2f5bff81ea9a35c
Saideep Chhetri <saideep_chhetri@yahoo.com>
2017-01-28 17:46:06 +0000

    Back Navigation

---
+++ src/pages/my-teams/my-teams.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { TournamentsPage } from '../pages'
+@Component({
+  selector: 'page-my-teams',
+  templateUrl: 'my-teams.html'
+})
+export class MyTeamsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MyTeamsPage');
+  }
+
+  goToTournaments(){
+    this.navCtrl.push(TournamentsPage);
+  }
+
+}
